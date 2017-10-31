0fa7e38cd53d88968cd8e045e15816db08460a63
Alexander <alexanderhle392@gmail.com>
2017-10-31 07:58:58 -0500

    Alex, Pritchett, Nick, Danny, Joseph
    Moved from team 6 to team 10

--- 1.3.9-10 sourceFiles/team10.py
+++ 1.3.9-10 sourceFiles/team10.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Waterbotle' # Only 10 chars displayed.
+strategy_name = 'Water em down'
+strategy_description = 'Like how water moves, unpredictable.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +27,9 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    possChoice = ('c','b')
+    choice = random.choice(possChoice)
+    return choice
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
