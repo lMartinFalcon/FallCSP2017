e05db1cbdd538bae1c5ff9ce0228fee23cc6544a
Ethan Nguyen <547143@nkcschools.org>
2017-10-31 07:41:02 -0500

    Team 5
    Uses a random numbers to decide choice.

--- 1.3.9-10 sourceFiles/team5.py
+++ 1.3.9-10 sourceFiles/team5.py
@@ -5,64 +5,50 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
+import random
+team_name = 'Team Five' # Only 10 chars displayed.
+strategy_name = 'Random'
+strategy_description = 'Random numbers dictate the outcome?'
+print 'function is move([your history], your score)'
+def move(their_history, their_score, my_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    choice1 = random.randint(0,10)
+    choice2 = random.randint(0,10)
+    choice3 = random.randint(0,10)
+    counter = 0;
+    decision = (choice1 + choice2 + choice3)/3
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+    conclusion = ''
     
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
+    if (decision >= 7):
+        print 'We Colude with you'
+        conclusion = 'c'
     else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+        print 'get betrayed kiddo'
+        conclusion = 'b'
+    while(counter < 2):
+        if (conclusion == 'c' and their_history[counter] == 'c'):
+            print ('WE outta there bby')
+            counter += 1
+        elif (conclusion == 'b' and their_history[counter] == 'b'):
+            print ("we both betrayed each other, rip 250 points")
+            counter += 1
+            their_score = their_score - 250
+            my_score = my_score - 250
+        elif (conclusion == 'b' and 'c'):
+            print "I betrayed you, plus 100 points for me"
+            counter += 1
+            my_score += 100
+        elif (their_history[counter] == 'b' and conclusion == 'c'):
+            print "You betrayed me, rip 500 points"
+            counter += 1
+            my_score -= 500
+            
+        print ('my score' + (str)(my_score))
+        print ('your score' + (str)(their_score))
+move()
